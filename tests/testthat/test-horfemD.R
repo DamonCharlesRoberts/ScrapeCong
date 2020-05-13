9f2cb5a039f3d1f60438366deb24f07cc7cc3642
Damon C. Roberts <damon.roberts-1@colorado.edu>
2020-05-13 09:54:02 -0600

    scrape

--- tests/testthat/test-horfemD.R
+++ tests/testthat/test-horfemD.R
@@ -2,9 +2,33 @@ context("horfemD")
 library(ScrapeCongress)
 test_that('horfemD', {
   skip_on_cran()
+  api_key <- "K9ArCyiCmOaJDTAArBXHDnGo0"
+  api_secret <- "1egMquUAtE9Kp5mDVmMomwfFIDrOZQcYoxUBcxUsZzEhtUnegw"
+  access_token <- "1114308687261261824-FEK7ybe6LwG4DFB4oofByZvouN6Jiz"
+  access_token_secret <- "C0sj3bXJOw3F5fapzMyDx0wkRZ8cR6rg1PNbRdnvnIJX3"
+  app_name <- "Congress_Gender"
+  key <- rtweet::create_token(
+    app = app_name,
+    consumer_key = api_key,
+    consumer_secret = api_secret,
+    access_token = access_token,
+    access_secret = access_token_secret
+  )
   expect_message(horfemD())
 })
 test_that('horfemD', {
   skip_on_cran()
+  api_key <- "K9ArCyiCmOaJDTAArBXHDnGo0"
+  api_secret <- "1egMquUAtE9Kp5mDVmMomwfFIDrOZQcYoxUBcxUsZzEhtUnegw"
+  access_token <- "1114308687261261824-FEK7ybe6LwG4DFB4oofByZvouN6Jiz"
+  access_token_secret <- "C0sj3bXJOw3F5fapzMyDx0wkRZ8cR6rg1PNbRdnvnIJX3"
+  app_name <- "Congress_Gender"
+  key <- rtweet::create_token(
+    app = app_name,
+    consumer_key = api_key,
+    consumer_secret = api_secret,
+    access_token = access_token,
+    access_secret = access_token_secret
+  )
   expect_warning(horfemD())
 })
