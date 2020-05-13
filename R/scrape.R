c41120c3a72128ca3bec5d001b7eefb5b823625c
Damon C. Roberts <damon.roberts-1@colorado.edu>
2020-05-13 16:05:06 -0600

    Update scrape.R for authentication bug fix

--- R/scrape.R
+++ R/scrape.R
@@ -40,20 +40,7 @@ library(readr)
 library(tm)
 library(utils)
 
-
-api_key <- readline(prompt = "Enter API_KEY: ")
-api_secret <- readline(prompt = "Enter API_SECRET: ")
-access_token <- readline(prompt = "Enter ACCESS_TOKEN: ")
-access_token_secret <- readline(prompt = "Enter ACCESS_TOKEN_SECRET: ")
-app_name <- readline(prompt = "Enter APP NAME: ")
-token <- create_token(
-  app = 'Congress_Gender',
-  consumer_key = api_key,
-  consumer_secret = api_secret,
-  access_token = access_token,
-  access_secret = access_token_secret
-)
-
+token <- rtweet::get_token()
 #############################
 #--------(1) senmaleD-------#
 #############################
